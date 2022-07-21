fac5ac7bb00b38d1004d9718b73c4f89c803f60d
马江伟 <1931967561@qq.com>
2022-07-22 00:05:30 +0800

    01

---
+++ renren-user/src/main/java/com/bw/user/service/UserService.java
@@ -0,0 +1,11 @@
+package com.bw.user.service;
+
+import com.bw.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+}
