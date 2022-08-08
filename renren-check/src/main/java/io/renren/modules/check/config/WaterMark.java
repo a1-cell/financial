64e8b706dec7da46e915fd8c22d9164f2e9da5d4
liuyh <2916435196@qq.com>
2022-08-08 10:21:53 +0800

    lyh0808

--- renren-check/src/main/java/io/renren/modules/check/config/WaterMark.java
+++ renren-check/src/main/java/io/renren/modules/check/config/WaterMark.java
@@ -1,116 +1,13 @@
 package io.renren.modules.check.config;
 
 import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.*;
-
-import javax.swing.*;
-import java.awt.*;
+import com.itextpdf.text.Image;
 import java.io.FileOutputStream;
-import java.io.IOException;
+
 
 public class WaterMark {
-//    public static void waterMark(PdfReader pdfReader, PdfStamper stamper, String waterMark) throws IOException, DocumentException {
-//        int interval = 50;
-//        BaseFont base = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.EMBEDDED);
-//        Rectangle pageRect = null;
-//        PdfGState gs = new PdfGState();
-//        //设置透明度
-//        gs.setFillOpacity(0.3f);
-//        gs.setStrokeOpacity(0.4f);
-//        int total = pdfReader.getNumberOfPages() + 1;
-//        JLabel label = new JLabel();
-//        FontMetrics metrics;
-//        int textH = 0;
-//        int textW = 0;
-//        label.setText(waterMark);
-//        metrics = label.getFontMetrics(label.getFont());
-//        textH = metrics.getHeight();
-//        textW = metrics.stringWidth(label.getText());
-//
-//        PdfContentByte under;
-//        for (int i = 1; i < total; i++) {
-//            pageRect = pdfReader.getPageSizeWithRotation(i);
-//            under = stamper.getOverContent(i);
-//            under.saveState();
-//            under.setGState(gs);
-//            under.beginText();
-//            under.setFontAndSize(base, 20);
-//            //设置水印颜色
-//            under.setColorFill(BaseColor.BLACK);
-//            // 水印文字成45度角倾斜
-//            for (int height = interval + textH; height < pageRect.getHeight(); height = height + textH * 3) {
-//                for (int width = interval + textW; width < pageRect.getWidth() + textW; width = width + textW * 2) {
-//                    under.showTextAligned(Element.ALIGN_LEFT, waterMark, width - textW, height - textH, 45);
-//                    break;
-//                }
-//                break;
-//            }
-//            // 添加水印文字
-//            under.endText();
-//        }
-//    }
-
-//    /**
-//     * @description
-//     *	给PDF文档添加水印
-//     * @author TianwYam
-//     * @date 2021年4月28日上午10:00:05
-//     */
-//    public static void addWaterMark(String pdfFilePath, String outputFilePath) {
-//
-//
-//        try {
-//            // 原PDF文件
-//            PdfReader reader = new PdfReader(pdfFilePath);
-//            // 输出的PDF文件内容
-//            PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(outputFilePath));
-//
-//            // 字体 来源于 itext-asian JAR包
-//            BaseFont baseFont = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", true);
-//
-//            PdfGState gs = new PdfGState();
-//            // 设置透明度
-//            gs.setFillOpacity(0.3f);
-//            gs.setStrokeOpacity(0.4f);
-//
-//            int totalPage = reader.getNumberOfPages() + 1;
-//            for (int i = 1; i < totalPage; i++) {
-//                // 内容上层
-////			PdfContentByte content = stamper.getOverContent(i);
-//                // 内容下层
-//                PdfContentByte content = stamper.getUnderContent(i);
-//
-//                content.beginText();
-//                // 字体添加透明度
-//                content.setGState(gs);
-//                // 添加字体大小等
-//                content.setFontAndSize(baseFont, 50);
-//                // 添加范围
-//                content.setTextMatrix(70, 200);
-//                // 具体位置 内容 旋转多少度 共360度
-//                content.showTextAligned(Element.ALIGN_CENTER, "机密文件", 300, 350, 300);
-//                content.showTextAligned(Element.ALIGN_TOP, "机密文件", 100, 100, 5);
-//                content.showTextAligned(Element.ALIGN_BOTTOM, "机密文件", 400, 400, 75);
-//
-//                content.endText();
-//            }
-//
-//            // 关闭
-//            stamper.close();
-//            reader.close();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-
-
-
-
 
     public static void addWaterMark(String srcPdfPath,String tarPdfPath,String WaterMarkContent)throws Exception {
         PdfReader reader = new PdfReader(srcPdfPath);
@@ -128,6 +25,29 @@ public class WaterMark {
             content.setColorFill(BaseColor.DARK_GRAY); //水印颜色
             content.setFontAndSize(font, 56); //水印字体样式和大小
             content.showTextAligned(Element.ALIGN_CENTER,WaterMarkContent, 350, 600, 30); //水印内容和水印位置
+
+            content.showTextAligned(Element.ALIGN_CENTER,WaterMarkContent, 270, 400, 30); //水印内容和水印位置
+
+            //添加图片
+            Image image = Image.getInstance("D:\\img\\oo.png");
+            Image image1 = Image.getInstance("D:\\img\\YE集团印.png");
+
+            /*
+              img.setAlignment(Image.LEFT | Image.TEXTWRAP);
+              img.setBorder(Image.BOX); img.setBorderWidth(10);
+              img.setBorderColor(BaseColor.WHITE); img.scaleToFit(100072);//大小
+              img.setRotationDegrees(-30);//旋转
+             */
+            //图片的位置（坐标）
+            image.setAbsolutePosition(520, 778);
+            image1.setAbsolutePosition(50, 680);
+            // image of the absolute
+            image.scaleToFit(200, 200);
+            image.scalePercent(15);//依照比例缩放
+            image1.scaleToFit(500, 500);
+            image1.scalePercent(35);//依照比例缩放
+            content.addImage(image);
+            content.addImage(image1);
             content.endText();
         }
         stamper.close();
--- renren-check/src/main/java/io/renren/modules/check/controller/PdfController.java
+++ renren-check/src/main/java/io/renren/modules/check/controller/PdfController.java
@@ -1,70 +1,98 @@
 package io.renren.modules.check.controller;
 
-import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfWriter;
 import io.renren.common.result.Result;
-import io.renren.modules.check.config.PdfUtil;
+
 import io.renren.modules.check.config.WaterMark;
+import io.renren.modules.check.pojo.TestPojo;
+import io.renren.modules.check.thread.PdfThread;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
 import java.io.OutputStream;
 import java.net.URLEncoder;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/pdf")
 public class PdfController {
-    @RequestMapping("/generate/{filename}")
-    public void generatePDF(HttpServletResponse response, @PathVariable String filename) throws Exception {
-//        String filename = "平等条约";
-        // 设置下载格式为pdf
-        response.setContentType("application/x-download");
-        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8") + ".pdf");
-        OutputStream os = new BufferedOutputStream(response.getOutputStream());
+    @RequestMapping("/generate/{filename}/{nn}/{oneS1}/{twoS2}")
+    public void generatePDF(HttpServletResponse response, @PathVariable String filename,@PathVariable String nn,@PathVariable String oneS1,@PathVariable String twoS2) throws Exception {
+        TestPojo testPojo = new TestPojo();
+        testPojo.setNn(nn);
+        testPojo.setOneS1(oneS1);
+        testPojo.setTwoS2(twoS2);
+        testPojo.setResponse(response);
+        testPojo.setFilename(filename);
+        List<TestPojo> list = new Vector<>();
+        list.add(testPojo);
+        long time1 = System.currentTimeMillis();
+//        System.out.println(""+time1);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        // 1. Document document = new Document();
-        Document document = PdfUtil.createDocument();
-        // 2. 获取writer
-        PdfWriter.getInstance(document, os);
-        // 3. open()
-        document.open();
+        PdfThread pdfThread = new PdfThread(list, countDownLatch);
 
-        //设置字体
-        Font blackFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLACK);
-        Font blueFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLUE);
-        Font bigFont = PdfUtil.createFont(14, Font.NORMAL, BaseColor.BLACK);
-        Font littleFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLACK);
+        Thread thread = new Thread(pdfThread);
+        thread.start();
+        countDownLatch.await();
 
-
-        String s="从2022年08月03日起,王少华欠刘宇航一套别墅，一辆豪车，一个媳妇，一个儿子，五年内还清，否则任由刘宇航处置，双方心甘情愿，无怨无悔。\n";
-        String s1="甲方：";
-        String s11="刘宇航\n";
-        String s2="乙方：";
-        String s22="王少华\n";
-        Chunk s111 = new Chunk(s11);
-        s111.setUnderline(5f, -10f);
-        Chunk s222 = new Chunk(s22);
-        s222.setUnderline(1f, -3f);
-        String sss=s1+s111+s2+s222;
-        Paragraph paragraph = PdfUtil.createParagraph(s, littleFont);
-        Paragraph paragraph1 = PdfUtil.createParagraph(sss, littleFont);
-        Paragraph title = PdfUtil.createParagraph("王刘平等条约", bigFont);
-        title.setAlignment(Element.ALIGN_CENTER);
-        paragraph.setFirstLineIndent(20);
-        // 4. 添加段落内容
-        document.add(title);
-        document.add(paragraph);
-        document.add(paragraph1);
-        // 5. close()
-        document.close();
-        os.close();
+        long time2 = System.currentTimeMillis();
+        System.out.println("耗时："+(time2-time1)+"ms");
+////        String filename = "平等条约";
+//        // 设置下载格式为pdf
+//        response.setContentType("application/x-download");
+//        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8") + ".pdf");
+//        OutputStream os = new BufferedOutputStream(response.getOutputStream());
+//
+//        // 1. Document document = new Document();
+//        Document document = PdfUtil.createDocument();
+//        // 2. 获取writer
+//        PdfWriter.getInstance(document, os);
+//        // 3. open()
+//        document.open();
+//
+//        //设置字体
+//        Font blackFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLACK);
+//        Font blueFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLUE);
+//        Font bigFont = PdfUtil.createFont(14, Font.NORMAL, BaseColor.BLACK);
+//        Font littleFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLACK);
+//
+//
+////        String s="从2022年08月03日起,王少华欠刘宇航一套别墅，一辆豪车，一个媳妇，一个儿子，五年内还清，否则任由刘宇航处置，双方心甘情愿，无怨无悔。\n";
+//        String s=nn;
+//        String s1="甲方：";
+//        String s11=oneS1+"\n";
+//        String s2="乙方：";
+//        String s22=twoS2+"\n";
+//        Chunk s111 = new Chunk(s11);
+//        s111.setUnderline(5f, -10f);
+//        Chunk s222 = new Chunk(s22);
+//        s222.setUnderline(1f, -3f);
+//        String sss=s1+s111+s2+s222;
+//        Paragraph paragraph = PdfUtil.createParagraph(s, littleFont);
+//        Paragraph paragraph1 = PdfUtil.createParagraph(sss, littleFont);
+//        Paragraph title = PdfUtil.createParagraph("王刘平等条约", bigFont);
+//        title.setAlignment(Element.ALIGN_CENTER);
+//        paragraph.setFirstLineIndent(20);
+//        // 4. 添加段落内容
+//        document.add(title);
+//        document.add(paragraph);
+//        document.add(paragraph1);
+//        // 5. close()
+//        document.close();
+//        os.close();
     }
 
     @RequestMapping("/hh/{filename}")
     public Result hh(@PathVariable String filename) throws Exception {
-        WaterMark.addWaterMark("E:\\浏览器下载的东西\\"+filename+".pdf","E:\\浏览器pdf水印\\"+filename+".pdf","人人开源");
+        WaterMark.addWaterMark("E:\\浏览器下载的东西\\"+filename+".pdf","E:\\浏览器pdf水印\\"+filename+".pdf","人人金融");
         return new Result(true,"生成水印成功",null);
     }
+
+
+
+
 }
